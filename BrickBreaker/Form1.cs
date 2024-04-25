e2652ca50377078b37ed9135f3b3288c9b804171
LogaGill489 <logagill489@ed.amdsb.ca>
2024-04-25 09:15:02 -0400

    Added changeScreen to Form1

--- BrickBreaker/Form1.cs
+++ BrickBreaker/Form1.cs
@@ -25,5 +25,28 @@ namespace BrickBreaker
 
             ms.Location = new Point((this.Width - ms.Width) / 2, (this.Height - ms.Height) / 2);
         }
+
+        public static void ChangeScreen(object sender, UserControl next)
+        {
+            Form f; // will either be the sender or parent of sender
+
+            if (sender is Form)
+            {
+                f = (Form)sender;                          //f is sender
+            }
+            else
+            {
+                UserControl current = (UserControl)sender;  //create UserControl from sender
+                f = current.FindForm();                     //find Form UserControl is on
+                f.Controls.Remove(current);                 //remove current UserControl
+            }
+
+            // add the new UserControl to the middle of the screen and focus on it
+            next.Location = new Point((f.ClientSize.Width - next.Width) / 2,
+                (f.ClientSize.Height - next.Height) / 2);
+            f.Controls.Add(next);
+            Cursor.Position = next.PointToScreen(new Point(next.Width / 2, (next.Height / 2) + 100));
+            next.Focus();
+        }
     }
 }
