e340c1c64841d81caf7d6dc8a6c07a99824ea143
Atticus Haslam <attihasl487@ed.amdsb.ca>
2024-05-09 21:00:46 -0400

    homoseual

--- BrickBreaker/Sound.cs
+++ BrickBreaker/Sound.cs
@@ -16,6 +16,8 @@ namespace BrickBreaker
             // When you enter a new level
             // Trigger.function
 
+            // Idle music, ambient, mostly Fallout tracks
+
             // Source root for sounds
             var soundsRoot = @"C:\Users\Attihasl487\Source\Repos\BrickBreakerTeam2\BrickBreaker\Resources\Sound";
             // Create new random
@@ -26,20 +28,55 @@ namespace BrickBreaker
             var playSound = soundFiles[rand.Next(0, soundFiles.Length)];
             // Play sound
             System.Media.SoundPlayer idle = new System.Media.SoundPlayer(playSound);
-         
-            // Create windows media player (2) for active sound
-            var active = new WindowsMediaPlayer();
-            active.URL = @"C:\Users\Attihasl487\Source\Repos\BrickBreakerTeam2\BrickBreaker\Resources\";
-            Console.ReadLine();
+
+            // Create windows media players for active sound
+            // Can hypothetically play 3 sounds at the same time, unless I should add more?
+            //var active = new WindowsMediaPlayer();
+            //active.URL = @"C:\Users\Attihasl487\Source\Repos\BrickBreakerTeam2\BrickBreaker\Resources\";
+            //var active2 = new WindowsMediaPlayer();
+            //active.URL = @"C:\Users\Attihasl487\Source\Repos\BrickBreakerTeam2\BrickBreaker\Resources\";
+            //var active3 = new WindowsMediaPlayer();
+            //active.URL = @"C:\Users\Attihasl487\Source\Repos\BrickBreakerTeam2\BrickBreaker\Resources\";
+            //var constant = new WindowsMediaPlayer();
+            //active.URL = @"C:\Users\Attihasl487\Source\Repos\BrickBreakerTeam2\BrickBreaker\Resources\";
+            //Console.ReadLine();
+
+            // Location of file
+            int butthole;
+            //List of files from directory
+            // Source root for sounds
+            var buttholesurfers = @"C:\Users\Attihasl487\Source\Repos\BrickBreakerTeam2\BrickBreaker\Resources\Sound"+butthole+"";
+            // list of files, by WAV
+            string[] activeFiles = Directory.GetFiles(@"C:\Users\Attihasl487\Source\Repos\BrickBreakerTeam2\BrickBreaker\Resources\", "*.wav");
+            // Something to change the given file to
+
+            var activePlay = soundFiles[butthole];
+            System.Media.SoundPlayer activebutthole = new System.Media.SoundPlayer(activePlay); 
+
+            //Either create a priority list, ensuring that the sound that occurs first is played on active, then active2, then active3...
+            //Or assign sounds likely to have conflict different active players... Which is easier.
 
             // While playing
 
             // If brick is struck, play brickstrike noise
+         
             // if brick is broken, play brickbroken noise
+
+            if ()
+            {
+                // Set int butthole to file location
+                butthole = "C:\Users\Attihasl487\Source\Repos\BrickBreakerTeam2\BrickBreaker\Resources\Sound";
+                // Call/reference back to buttholesurfers, should replace int 'butthole' with the given soundfile location, much like with the weather app
+
+            }
+
             // if paddle is hit, play paddlehit noise
+      
             // if wall is struck, play wallstrike noise
+         
             // If life is subtracted
-            // If HP < 2
+        
+            // While HP < 2
 
             //If button clicked
 
